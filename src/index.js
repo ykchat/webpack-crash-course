f1877ac6a04f91ae2883bf814febc49af149fb5c
ykchat <kakutani@qj9.so-net.ne.jp>
2022-08-22 12:01:48 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import _ from 'lodash';
+import { NiJou } from './utilities';
+import { NAME as NAME_OF_HAM } from './utilities';
+import Tiger from './utilities';
+
+console.log(NiJou(3));
+console.log(NAME_OF_HAM);
+console.log(Tiger.say());
 
 function componet() {
     const element = document.createElement('div');
