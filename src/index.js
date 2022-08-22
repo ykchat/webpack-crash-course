bb5f924aa3178af2c7da30e060b1fc0d071a188c
ykchat <kakutani@qj9.so-net.ne.jp>
2022-08-22 11:15:49 +0900

    install webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 function componet() {
     const element = document.createElement('div');
-    const array = ['Hello', 'webpack'];
+    const array = ['Hello', 'webpack', '!!'];
     element.innerHTML = _.join(array, ' ');
     return element;
 }
