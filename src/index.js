ee7c17b74b7e965862301b352f80af8557b5180e
ykchat <kakutani@qj9.so-net.ne.jp>
2022-08-23 14:42:44 +0900

    implement babel-loader and react development

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-import _ from 'lodash';
-import './style.css';
-import './style.scss';
-import logo from './logo.png';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function componet() {
-    const element = document.createElement('div');
-    const array = ['Hello', 'webpack', '!!'];
-    element.innerHTML = _.join(array, ' ');
-    return element;
-}
-
-function image() {
-    const image = new Image();
-    image.src = logo;
-    return image;
-}
-
-document.body.appendChild(componet());
-document.body.classList.add('haikei');
-document.body.appendChild(image());
+ReactDOM.render(
+    <div>Hello, React!</div>,
+    document.getElementById('root')
+)
